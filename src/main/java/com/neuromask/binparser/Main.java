2f71ea8871154bc871347ed339607bdc1a2252bb
nizam <keramov-nizam@yandex.ru>
2025-02-11 14:12:42 +0300

    refactored Main class

--- src/main/java/com/neuromask/binparser/Main.java
+++ src/main/java/com/neuromask/binparser/Main.java
@@ -3,13 +3,12 @@ package com.neuromask.binparser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 
@@ -22,50 +21,56 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("log4j2.configurationFile",
                 String.valueOf(NeuromaskBinParser.class.getClassLoader().getResource("log4j2.xml")));
-        Path inputPath = Paths.get(INPUT_PATH);
-        if (Files.isDirectory(inputPath)) {
-            Path outputPath = Paths.get(OUTPUT_PATH);
-            if (!Files.exists(outputPath)) {
-                try {
-                    Files.createDirectories(outputPath);
-                }
-                catch (IOException e) {
-                    LOGGER.error("Output dir could not be created");
-                }
-            }
-            if (Files.isDirectory(outputPath)) {
-                processDirFiles(INPUT_PATH);
-            }
+        Path inputDirPath = Paths.get(INPUT_PATH);
+        Path outputDirPath = Paths.get(OUTPUT_PATH);
+
+        if (!Files.isDirectory(inputDirPath)) {
+            LOGGER.error("Input directory '{}' does not exist.", inputDirPath);
+            return;
         }
-        else
-            LOGGER.error("Input dir does not exist");
+
+        createOutputDirectory(outputDirPath);
+
+        if (!Files.isDirectory(outputDirPath)) {
+            LOGGER.error("Could not create output directory '{}'", outputDirPath);
+            return;
+        }
+
+        processBinFiles(inputDirPath, outputDirPath);
+
     }
 
-    private static void processDirFiles(String dirPath){
-        File dir = new File(dirPath);
-        File[] fileList = dir.listFiles();
-        if (fileList != null) {
-            for (File file : fileList) {
-                String fileName = file.getName();
-                if (fileName.toLowerCase().endsWith(".bin"))
-                    try (InputStream in = Files.newInputStream(Paths.get(INPUT_PATH + fileName))) {
-                        List<String> records = NeuromaskBinParser.parseStream(in, NeuromaskBinParser.DEFAULT_FLOAT_PRECISION);
-                        String newFileName = fileName.toLowerCase().replace("bin", "json");
-                        try (FileWriter writer = new FileWriter(OUTPUT_PATH + newFileName)) {
-                            for(String str: records) {
-                                writer.write(str + System.lineSeparator());
-                            }
-                            LOGGER.info(String.format("BIN-file %s successfully processed, data written to JSON-file %s", fileName, newFileName));
-                        }
-                        catch(Exception e) {
-                            LOGGER.error("JSON-file writing error {}", e.getMessage());
-                        }
-                        LOGGER.info(String.format("BIN-file %s successfully processed", fileName));
-                    }
-                    catch(IOException e) {
-                        LOGGER.error(String.format("BIN-file %s reading error", fileName), e.getMessage());
-                    }
-            }
+    private static void processBinFiles(Path inputDirPath, Path outputDirPath) {
+        try {
+            Files.list(inputDirPath)
+                    .filter(path -> path.toString().toLowerCase().endsWith(".bin"))
+                    .forEach(binFile -> processBinFile(binFile, outputDirPath));
+        } catch (IOException e) {
+            LOGGER.error("Error processing files in input directory: {}", e.getMessage());
+
+        }
+    }
+
+    private static void processBinFile(Path binFile, Path outputDirPath) {
+        LOGGER.info("BIN-file '{}' processing started", binFile);
+        String fileName = binFile.getFileName().toString();
+        String jsonFileName = fileName.toLowerCase().replace(".bin", ".json");
+        Path jsonFilePath = outputDirPath.resolve(jsonFileName);
+        try (InputStream in = Files.newInputStream(binFile)) {
+            List<String> records = NeuromaskBinParser.parseStream(in, NeuromaskBinParser.DEFAULT_FLOAT_PRECISION);
+            Files.write(jsonFilePath, records, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            LOGGER.info("BIN-file '{}' successfully processed, data written to JSON-file '{}'", fileName,
+                    jsonFilePath);
+        } catch (IOException e) {
+            LOGGER.error("Error processing BIN-file '{}': {}", fileName, e.getMessage());
+        }
+    }
+
+    private static void createOutputDirectory(Path outputDirPath) {
+        try {
+            Files.createDirectories(outputDirPath);
+        } catch (IOException e) {
+            LOGGER.error("Error creating output directory: {}", e.getMessage());
         }
     }
 }
