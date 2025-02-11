9d7b64f1a065705445b1a3d85a0e80cc2bda8959
nizam <keramov-nizam@yandex.ru>
2025-02-11 14:13:27 +0300

    refactored NeuromaskBinParserTest class

--- src/test/java/NeuromaskBinParserTest.java
+++ src/test/java/NeuromaskBinParserTest.java
@@ -2,30 +2,41 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.neuromask.binparser.NeuromaskBinParser;
 import org.junit.Test;
-
-import static net.javacrumbs.jsonunit.JsonAssert.assertJsonEquals;
-import static net.javacrumbs.jsonunit.JsonAssert.when;
-import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
-import static org.junit.Assert.assertEquals;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class NeuromaskBinParserTest {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     private void testFile(String fileName) throws IOException{
         InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
         List<String> parseRecords = NeuromaskBinParser.parseStream(in, NeuromaskBinParser.DEFAULT_FLOAT_PRECISION);
+
+        // Чтение ожидаемых данных из JSON-файла
         String newFileName = fileName.toLowerCase().replace("bin", "json");
         BufferedReader br = new BufferedReader(new FileReader("src/test/resources/" + newFileName));
-        List<String> expectedRecords = new ArrayList<>();
         String line;
+        ArrayNode expectedJson = objectMapper.createArrayNode();
         while ((line = br.readLine()) != null) {
-            expectedRecords.add(line);
+            expectedJson.add(objectMapper.readTree(line));
         }
         br.close();
-        assertEquals(parseRecords, expectedRecords);
-        assertJsonEquals(parseRecords, expectedRecords, when(IGNORING_ARRAY_ORDER));
+
+        // Преобразование списка строк в массив JSON-объектов
+        ArrayNode actualJson = objectMapper.createArrayNode();
+        for (String record : parseRecords) {
+            actualJson.add(objectMapper.readTree(record));
+        }
+
+        assertEquals(expectedJson, actualJson);
     }
 
     @Test
